968f32bd05a3a270e47708088718c67a886b789d
Matheus Chonge <matheus.chong@gmail.com>
2022-05-27 14:57:28 -0300

    feat create categorie

--- src/controllers/categoriesController/index.js
+++ src/controllers/categoriesController/index.js
@@ -6,14 +6,26 @@ export default class CategoriesController {
         try {
             const db = await connectDB()
             const categories = await db.query('SELECT * FROM categories');
-            res.status(200).send(categories.rows )
+            res.status(200).send(categories.rows)
         } catch (e) {
-            res.status(500).json({message: 'error', e})
+            res.status(500).json({ message: 'error', e })
         }
-        
+
     }
 
     async createCategory(req, res) {
-        res.status(200).json({ message: 'createCategory' })
+        try {
+            const { name } = req.body
+            if (!name) return res.status(400).json({ message: 'name cannot be null', status: 400 })
+
+            const db = await connectDB()
+            const create = await db.query('INSERT INTO categories (name) VALUES ($1)',[name])
+
+            res.status(201).json({ message: 'createdCategory' })
+
+        } catch (error) {
+            res.status(500).json({ message: 'error durin Creation', error })
+        }
+
     }
 }
